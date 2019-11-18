b5653ac43cd62c0e56b7d0b81f59ad87c80e8324
Maria Clemmensen <clemmensen.maria@gmail.com>
2019-11-18 10:52:43 +0100

    Intro - Part 1, added statuscodes

--- Intro/server.js
+++ Intro/server.js
@@ -8,10 +8,13 @@ var con = mysql.createConnection({
     database: "h3_serverside_programming"
 });
 
+
+
 var server = http.createServer(function(req, res) {
     con.connect(function(err) {
 
         if (err != null && err.code == "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
+            res.statusCode = 404;
             res.write("An error occured when connecting to the database");
             res.end();
         }
@@ -20,6 +23,7 @@ var server = http.createServer(function(req, res) {
             con.query("SELECT * FROM names", function(err, data) {
 
                 if (err != null && err.code == "ER_NO_SUCH_TABLE") {
+                    res.statusCode = 404;
                     res.write("The expected table was not found");
                     res.end();
                 }
