a4020439c0eda08fde855030490c6e7f51ac1dcb
Maria Clemmensen <clemmensen.maria@gmail.com>
2019-11-28 12:50:34 +0100

    REST API - Issue 5, let users request an api key

--- REST-API/Controllers/Pages/_pageController.js
+++ REST-API/Controllers/Pages/_pageController.js
@@ -2,6 +2,8 @@ var handleError = require("../Errors/errorController.js").handleError;
 
 // Controllers for url end points
 var apiPages = {
+    "/": require("./apiKeyController"),
+    "/api-key": require("./apiKeyController"),
     "/rooms": require("./roomsController"),
     "/bookings": require("./bookingsController")
 }
@@ -11,7 +13,7 @@ exports.handleQuery = (req, res, conn) => {
 
     // Check if main page handler is defined - using high url path selection
     var baseApiSelector = "/" + req.url.split("?")[0].split("/")[1];
-    
+
     if (!(baseApiSelector in apiPages)) handleError(req, res, 3, "Page '" + baseApiSelector + "' was not found as a valid endpoint");
     else apiPages[baseApiSelector].controller(req, res, conn);
 }
